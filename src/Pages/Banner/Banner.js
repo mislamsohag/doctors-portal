4974ff063c72020594ad89f654fb645bc30cc45e
soahg <mislamsohag@gmail.com>
2022-05-12 07:18:52 +0600

    hero part add

--- src/Pages/Banner/Banner.js
+++ src/Pages/Banner/Banner.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import chair from '../../assets/images/chair.png';
 
 const Banner = () => {
     return (
         <>
             <div>
-                <div className="hero min-h-screen bg-base-200">
+                <div className="hero min-h-screen">
                     <div className="hero-content flex-col lg:flex-row-reverse">
-                        <img src="https://api.lorem.space/image/movie?w=260&h=400" className="max-w-sm rounded-lg shadow-2xl" />
+                        <img src={chair} className="max-w-sm rounded-lg shadow-2xl" />
                         <div>
                             <h1 className="text-5xl font-bold">Box Office News!</h1>
                             <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
