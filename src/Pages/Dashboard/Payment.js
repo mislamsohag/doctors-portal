3b676c8ec97401ee4b2e6e74046ae949ea8df983
soahg <mislamsohag@gmail.com>
2022-05-23 15:52:52 +0600

    url update

--- src/Pages/Dashboard/Payment.js
+++ src/Pages/Dashboard/Payment.js
@@ -1,32 +1,44 @@
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import React from 'react';
-import { useParams } from 'react-router-dom';
 import { useQuery } from 'react-query';
+import { useParams } from 'react-router-dom';
 import Loading from '../../Sheard/Loading';
+
+import CheckoutForm from './CheckoutForm';
+
+const stripePromise = loadStripe('pk_test_51L2TF0CWvLG0jlvbtUSAlV7HpkXuCXJ3XPOr40Mk6P4npNRzsemvWfqYhH3kjpbkrpSXAl7AjVosxA0IN3u3VRoT00fWgngE2D');
 
 const Payment = () => {
     const { id } = useParams();
-    const url = (`https://boiling-plains-67002.herokuapp.com/booking/${id}`);
+    const url = `https://boiling-plains-67002.herokuapp.com/booking/${id}`;
+
     const { data: appointment, isLoading } = useQuery(['booking', id], () => fetch(url, {
         method: 'GET',
         headers: {
             'authorization': `Bearer ${localStorage.getItem('accessToken')}`
         }
-    }
-    ).then(res => res.json()));
+    }).then(res => res.json()));
 
     if (isLoading) {
         return <Loading></Loading>
     }
 
-
     return (
         <div>
-            <div class="card w-50 max-w-md bg-base-100 shadow-xl my-12">
-                <div class="card-body">
+            <div className="card w-50 max-w-md bg-base-100 shadow-xl my-12">
+                <div className="card-body">
                     <p className="text-success font-bold">Hello, {appointment.patientName}</p>
-                    <h2 class="card-title">Please Pay for {appointment.treatment}</h2>
+                    <h2 className="card-title">Please Pay for {appointment.treatment}</h2>
                     <p>Your Appointment: <span className='text-orange-700'>{appointment.date}</span> at {appointment.slot}</p>
                     <p>Please pay: ${appointment.price}</p>
+                </div>
+            </div>
+            <div className="card flex-shrink-0 w-50 max-w-md shadow-2xl bg-base-100">
+                <div className="card-body">
+                    <Elements stripe={stripePromise}>
+                        <CheckoutForm appointment={appointment} />
+                    </Elements>
                 </div>
             </div>
         </div>
